f13c5703229603b92483e5c9cbcac6160674a186
Tobias Feldballe <ext_tfpe@mir-robots.com>
2024-08-04 23:20:58 +0200

    Removed some non needed stuff

--- routes/routes.go
+++ routes/routes.go
@@ -12,23 +12,9 @@ import (
 	"github.com/tbaehler/gin-keycloak/pkg/ginkeycloak"
 )
 
-var (
-	masterClientID        = "admin-cli"
-	masterClientSecret    = "a923d503-0398-4d56-ab1c-9d0a4bd63bd6"
-	masterClientSecretDev = "472b0199-caef-4cc4-9a37-3f44e687d5ec"
-	clientID              = "lttr"
-	clientSecret          = "c0cb0fe2-4b2a-4a20-b0c3-879aa0279fb6"
-	clientSecretDev       = "fc8b269b-e7e5-428c-9819-5f03f39f3acc"
-)
-
-//StartGin function
+// StartGin function
 func StartGin() {
 	config := config.GetConfig()
-	production := config.GetBool("production")
-	if !production {
-		masterClientSecret = masterClientSecretDev
-		clientSecret = clientSecretDev
-	}
 
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
